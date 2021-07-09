696ac1e38f967b534b25681f902e468df8569bd0
CodeManiac1991 <86159923+CodeManiac1991@users.noreply.github.com>
2021-07-09 20:58:12 +0300

    Примеры конструкторов
    Будет добавляться и обновляться

---
+++ Constructors/main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include "cnstrs.cpp"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    Example_1 e1 {2,3}; //ПРИМЕР СПИСКА ИНИЦИАЛИЗАЦИИ
+
+    //КОНСТРУКТОР КОПИРОВАНИЯ
+    Example_3 e3(0,0);
+    Example_3 e3_1(e3);
+
+    Example_4 e4;
+
+    return a.exec();
+}
